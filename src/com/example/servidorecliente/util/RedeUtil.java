a5fc05065932ca3ba985516985b5ae28310a9b26
Hamilton Lima <hlima@vacavitoria.com>
2013-05-17 09:55:38 -0300

    correcao no mecanismo de conexao, em android 4 que estava retornando endereco de rede em IPV6

--- src/com/example/servidorecliente/util/RedeUtil.java
+++ src/com/example/servidorecliente/util/RedeUtil.java
@@ -1,5 +1,6 @@
 package com.example.servidorecliente.util;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -12,6 +13,8 @@ public class RedeUtil {
 	private static final String TAG = RedeUtil.class.getName();
 
 	public static String getLocalIpAddress() {
+		String result = null;
+
 		try {
 			Enumeration<NetworkInterface> en = NetworkInterface
 					.getNetworkInterfaces();
@@ -19,19 +22,28 @@ public class RedeUtil {
 			while (en.hasMoreElements()) {
 				NetworkInterface intf = en.nextElement();
 				Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses();
+				
+				Log.i(TAG, "NI=" + intf);
 
 				while (enumIpAddr.hasMoreElements()) {
 
 					InetAddress inetAddress = enumIpAddr.nextElement();
-					if (!inetAddress.isLoopbackAddress()) {
-						return inetAddress.getHostAddress().toString();
+
+					Log.i(TAG, "ADDR=" + inetAddress);
+
+					if (!inetAddress.isLoopbackAddress()
+							&& (inetAddress instanceof Inet4Address)) {
+						result = inetAddress.getHostAddress().toString();
+						Log.i(TAG, "RSLT=" + result);
+
 					}
 				}
 			}
 		} catch (SocketException ex) {
 			Log.e(TAG, "erro recuperando IP atual", ex);
 		}
-		return null;
+
+		return result;
 	}
 
 }
